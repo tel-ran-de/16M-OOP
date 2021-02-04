55b60f056a3e170cc59c8906f3f7d55479999adb
andron13 <andron13@gmail.com>
2021-02-04 11:34:28 +0100

    L2

---
+++ 2021_02_04_Constructor_And_This/src/Article.java
@@ -0,0 +1,30 @@
+/**
+ * 16M-OOP
+ * 04 10 : 56
+ */
+public class Article {
+    String title;
+    String announce;
+    String fullText;
+    int id;
+
+/*    public Article() {
+        title = "unknown";
+        announce = "Напиши здесь анонс";
+        fullText = "unknown";
+    }*/
+
+    public Article(String articleTitle, String articleAnnounce) {
+        title = articleTitle;
+        announce = articleAnnounce;
+    }
+
+    public Article(int articleID,
+                   String articleTitle,
+                   String articleAnnounce) {
+        title = articleTitle;
+        announce = articleAnnounce;
+        id = articleID;
+    }
+}
+
