7b891d7d968d5a060830e6317d5af51f9a6b49fe
andron13 <andron13@gmail.com>
2021-02-04 13:31:49 +0100

    this done

--- 2021_02_04_Constructor_And_This/src/Rectangle.java
+++ 2021_02_04_Constructor_And_This/src/Rectangle.java
@@ -3,17 +3,31 @@
  * 04 13 : 05
  */
 public class Rectangle {
-    int x;
+    int x, y;
+    int width, height;
 
-    public Rectangle(int x) {
+    public Rectangle(int x, int y, int width, int height) {
         this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
+
+    public Rectangle(int width, int height) {
+/*        x = 0;
+        y = 0;
+        this.width = width;
+        this.height = height;*/
+        // new Rectangle(0, 0, width, height);
+        this(0, 0, width, height);
+    }
+
 }
 
 
 class RectangleTest {
     public static void main(String[] args) {
-        Rectangle firstRectangle = new Rectangle(100);
-        Rectangle secondRectangle = new Rectangle(50);
+        Rectangle firstRectangle = new Rectangle(500, 700);
+        Rectangle secondRectangle = new Rectangle(200, 250, 400, 800);
     }
 }
